5b46cb92f24b75341b99f12df1aebef59b082d8a
Timplay <114745455+Timplaue@users.noreply.github.com>
2025-03-01 11:28:04 +0900

    kt-3 update 11:27

--- server/newsUpdater.js
+++ server/newsUpdater.js
@@ -9,19 +9,19 @@ mongoose.connect("mongodb://localhost:27017/patriot", {
 
 const news = [
     {
-        title: "Новая новость 1",
-        content: "Это содержимое первой новости.",
-        imgUrl: "https://avatars.mds.yandex.net/i?id=679720f2a94327342bb6a0e160ce7bb8_l-8497208-images-thumbs&n=13",
+        title: "Россия и наши союзники. Проблем меньше не будет",
+        content: "Читать далее",
+        imgUrl: "/images/news4.png",
     },
     {
-        title: "Новая новость 2",
-        content: "Это содержимое второй новости.",
-        imgUrl: "https://example.com/image2.jpg",
+        title: "28 февраля: какой сегодня праздник, что отмечают в России и мире?",
+        content: "Читать далее",
+        imgUrl: "/images/news5.png",
     },
     {
-        title: "Новая новость 3",
-        content: "Это содержимое третьей новости.",
-        imgUrl: "https://example.com/image3.jpg",
+        title: "Отключение Skype официально подтвердили в Microsoft",
+        content: "Читать далее",
+        imgUrl: "/images/news6.png",
     },
 ];
 
